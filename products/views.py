dafa86d9c0eea4709d0ad2c8cbdc6d34f76f7827
Olga <olga@gmail.com>
2021-02-16 00:54:43 +0300

    Added subtract for Cart, pagination, no_image image

--- products/views.py
+++ products/views.py
@@ -10,20 +10,19 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-    # paginator = Paginator(products, 10)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    # if 'page' in request.GET:
-    #     page_num = request.GET['page']
-    # else:
-    #     page_num = 1
-    # page = paginator.get_page(page_num)
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+    paginator = Paginator(products, 12)
+    page = paginator.get_page(page_num)
     cart_product_form = CartAddProductForm()
 
     content = {'category': category,
                'categories': categories,
-               'object_list': products,
-               # 'page': page,
+               'page_object': page,
                'cart_product_form': cart_product_form}
     return render(request, 'products/list.html', content)
