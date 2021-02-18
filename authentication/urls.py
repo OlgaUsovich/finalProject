247f07094b506f3825020df4820991e32620035a
Olga <olga@gmail.com>
2021-02-18 22:35:55 +0300

    Added bootstrap4_forms, corrected template name

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,8 +5,9 @@ from authentication.views import CreateUserView, view_profile, profile_edit, cha
 app_name = 'authentication'
 urlpatterns = [
     path('reg/', CreateUserView.as_view(), name='registration'),
-    path('login/', LoginView.as_view(template_name='login.html', success_url='{% url "authentication:profile" %}',
-                                     redirect_authenticated_user=True), name='login'),
+    path('login/',
+         LoginView.as_view(template_name='authentication/login.html', success_url='{% url "authentication:profile" %}',
+                           redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('profile/change_password/', change_password, name='change_password'),
     path('profile/<int:id>/edit/', profile_edit, name='edit_profile'),
