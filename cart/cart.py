df724ed8caed6dfe63c625e223814be1a23dbc32
Olga <olga@gmail.com>
2021-02-25 20:51:34 +0300

    Fixed link from cart to product. Added function get_label to cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -1,25 +1,20 @@
 from decimal import Decimal
 from django.conf import settings
+from django.urls import reverse
+
 from products.models import Product
 
 
 class Cart:
 
     def __init__(self, request):
-        """
-        Инициализируем корзину
-        """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, product, quantity=1, update_quantity=False):
-        """
-        Добавить продукт в корзину или обновить его количество.
-        """
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0,
@@ -31,26 +26,17 @@ class Cart:
         self.save()
 
     def save(self):
-        # Обновление сессии cart
         self.session[settings.CART_SESSION_ID] = self.cart
-        # Отметить сеанс как "измененный", чтобы убедиться, что он сохранен
         self.session.modified = True
 
     def remove(self, product):
-        """
-        Удаление товара из корзины.
-        """
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
 
     def __iter__(self):
-        """
-        Перебор элементов в корзине и получение продуктов из базы данных.
-        """
         product_ids = self.cart.keys()
-        # получение объектов product и добавление их в корзину
         products = Product.objects.filter(id__in=product_ids)
         for product in products:
             self.cart[str(product.id)]['product'] = product
@@ -61,19 +47,21 @@ class Cart:
             yield item
 
     def __len__(self):
-        """
-        Подсчет всех товаров в корзине.
-        """
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
-        """
-        Подсчет стоимости товаров в корзине.
-        """
         return sum(Decimal(item['price']) * item['quantity'] for item in
                    self.cart.values())
 
     def clear(self):
-        # удаление корзины из сессии
         del self.session[settings.CART_SESSION_ID]
         self.session.modified = True
+
+    def get_label(self):
+        quantity = sum(item['quantity'] for item in self.cart.values())
+        if quantity % 10 == 1 and quantity % 100 != 11:
+            return 'товар'
+        elif quantity in (2, 3, 4) and quantity not in (12, 13, 14):
+            return 'товара'
+        else:
+            return 'товаров'
